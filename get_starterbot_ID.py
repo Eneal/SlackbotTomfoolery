54ab6422fbb907c52fbd1a5e9177bd51cc88e574
Ezra Neal <eneal@q2ebanking.com>
2017-04-05 15:46:33 -0500

    initial commit. Starterbot tutorial complete

---
+++ get_starterbot_ID.py
@@ -0,0 +1,19 @@
+from slackclient import SlackClient
+
+
+BOT_NAME = 'starterbot'
+STARTER_BOT_TOKEN = "xoxb-164308173152-Sd1XL4SAahvDsSwxrvPQtO4V"
+
+slack_client = SlackClient(STARTER_BOT_TOKEN)
+
+if __name__ == "__main__":
+    api_call = slack_client.api_call("users.list")
+    print api_call
+    if api_call.get("ok"):
+        # retrieve all users so we can find our bot
+        users = api_call.get("members")
+        for user in users:
+            if "name" in user and user.get("name") == BOT_NAME:
+                print "Bot ID for '" + user["name"] + "'is " + user.get("id")
+    else:
+        print "could not find the bot user with name " + BOT_NAME
